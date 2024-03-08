6564f6f375f48d9f3310081cace8abac34e98c75
kaliev1910 <nelih98840@lieboe.com>
2024-03-08 18:27:54 +0600

    initial commit

---
+++ src/main/java/com/example/java_19_headhunter/Java19HeadHunterApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_19_headhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java19HeadHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java19HeadHunterApplication.class, args);
+    }
+
+}
