c6fccd017f67f5ddf68d84eee173da00a2cc567e
kaliev1910 <ulukbek.kaliev.01@mail.ru>
2024-03-15 18:40:53 +0600

    add methods for ContactInfo

--- src/main/java/com/example/java_19_headhunter/dao/interfaces/ContactInfoDao.java
+++ src/main/java/com/example/java_19_headhunter/dao/interfaces/ContactInfoDao.java
@@ -1,4 +1,15 @@
 package com.example.java_19_headhunter.dao.interfaces;
 
+import com.example.java_19_headhunter.models.ContactInfo;
+
+import java.util.List;
+
 public interface ContactInfoDao {
+    void insert(ContactInfo contactInfo);
+
+    void update(ContactInfo contactInfo);
+
+    List<ContactInfo> findByResumeId(int resumeId);
+
+    void deleteByResumeId(int resumeId);
 }
