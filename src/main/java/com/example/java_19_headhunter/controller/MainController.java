132a79a96632ce79c5d41c5809777adb463c7072
kaliev1910 <ulukbek.kaliev.01@mail.ru>
2024-05-10 16:05:01 +0600

    login validation fix

--- src/main/java/com/example/java_19_headhunter/controller/MainController.java
+++ src/main/java/com/example/java_19_headhunter/controller/MainController.java
@@ -31,13 +31,14 @@ public class MainController {
     private final ResponseService responseService;
 
     @GetMapping("/register")
-    public String showRegistrationForm(BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("user", model);
-            return "auth/register";
+    public String showRegistrationForm(@RequestParam(defaultValue = "false", required = false) Boolean success,
+                                       Model model) {
+        if (success.equals(Boolean.TRUE)) {
+            return "redirect:/login";
         }
 
-        return "auth/login";
+
+        return "auth/register";
     }
 
     @PostMapping("/register")
@@ -53,14 +54,11 @@ public class MainController {
         }
 
         userService.createUser(userDto);
-        return "redirect:/register?success";
+        return "redirect:/register?success=true";
     }
 
     @GetMapping("/login")
-    public String login(
-            @RequestParam(defaultValue = "false", required = false) Boolean error,
-            Model model
-    ) {
+    public String login(@RequestParam(defaultValue = "false", required = false) Boolean error, Model model) {
         if (error.equals(Boolean.TRUE)) {
             model.addAttribute("error", "Invalid Username or Password");
         }
