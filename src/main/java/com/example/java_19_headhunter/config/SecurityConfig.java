9ca203744eb9ed72af9d6d3e5e836247bede3ef0
kaliev1910 <ulukbek.kaliev.01@mail.ru>
2024-05-16 17:25:43 +0600

    SecurityConfig crsf disabled

--- src/main/java/com/example/java_19_headhunter/config/SecurityConfig.java
+++ src/main/java/com/example/java_19_headhunter/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
 //                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
 //                .formLogin(AbstractHttpConfigurer::disable)
