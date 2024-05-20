4e198fb2370de966114c4d52c5bd26d9e1b6241b
kaliev1910 <ulukbek.kaliev.01@mail.ru>
2024-05-21 05:08:14 +0600

    can remove but not add

--- src/main/java/com/example/java_19_headhunter/repository/ContactInfoRepository.java
+++ src/main/java/com/example/java_19_headhunter/repository/ContactInfoRepository.java
@@ -1,7 +1,9 @@
 package com.example.java_19_headhunter.repository;
 
 import com.example.java_19_headhunter.models.ContactInfo;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.List;
 
@@ -12,8 +14,9 @@ public interface ContactInfoRepository extends JpaRepository<ContactInfo, Intege
 
     List<ContactInfo> findContactInfosByResumeId_Id(int resumeId);
 
-    
 
+    @Modifying
+    @Transactional
     void deleteByResumeId_Id(int resumeId);
 
 }
