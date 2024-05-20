916255f2fd825194158ca608b74777eac7996717
kaliev1910 <ulukbek.kaliev.01@mail.ru>
2024-05-21 01:13:17 +0600

    added dropdown menu to change language

--- src/main/java/com/example/java_19_headhunter/config/AppConfig.java
+++ src/main/java/com/example/java_19_headhunter/config/AppConfig.java
@@ -4,8 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class AppConfig {
-
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
 }
